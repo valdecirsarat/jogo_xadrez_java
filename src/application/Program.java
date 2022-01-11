698d7235fa808aa9f8724e8ff25554b31da46930
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-01-11 09:35:59 -0300

    impressao do tabuleiro no console

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,15 @@
 package application;
 
 import boardgame.Board;
+import chess.ChessMath;
 
 
 public class Program {
 
 	public static void main(String[] args) {
-		Board board = new Board(8, 8);
-		System.out.println(board);
+		ChessMath chessMath = new ChessMath();
+		UI.printBoard(chessMath.getPieces());
+		
 
 	}
 
