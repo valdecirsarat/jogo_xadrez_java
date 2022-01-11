6cc6dc5fc5f564f6951cc3a2038e77bb7439ac81
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-01-11 10:37:10 -0300

    Criando excecoes

--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -24,7 +24,7 @@ public class ChessMath {
 	
 	private void initialSetup() {
 		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(board, Color.WHITE), new Position(0, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(4, 1));
 		board.placePiece(new Rook(board, Color.BLACK), new Position(7, 4));
 	}
 
