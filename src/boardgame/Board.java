9c6893bea24bddab45118711a96e52efa160cd4d
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-01-11 16:33:59 -0300

    movendo peças

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -45,6 +45,23 @@ public class Board {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Erro: Posição não existe");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+		
+	}
+	
+	
+	
+	
 	
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
