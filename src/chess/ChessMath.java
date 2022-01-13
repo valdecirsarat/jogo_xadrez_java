a8d80bedaec76b01e98e2cb504c546d7db26cdde
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-01-13 20:01:07 -0300

    Rainha

--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -11,6 +11,7 @@ import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knigth;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMath {
@@ -198,18 +199,16 @@ public class ChessMath {
 	}
 	
 	private void initialSetup() {
-		//torre
+		// Brancas
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('b', 1, new Knigth(board, Color.WHITE));
+		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('g', 1, new Knigth(board, Color.WHITE)); 
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
-        //cavalo
-        placeNewPiece('b', 1, new Knigth(board, Color.WHITE));
-        placeNewPiece('g', 1, new Knigth(board, Color.WHITE));
-        //bispo
-        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));        
-        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
-        //rei
-        placeNewPiece('e', 1, new King(board, Color.WHITE));
-        //peao
+        
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
@@ -219,18 +218,16 @@ public class ChessMath {
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
         
-        //torre
+       //Pretas
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
-        //cavalo
         placeNewPiece('b', 8, new Knigth(board, Color.BLACK));
-        placeNewPiece('g', 8, new Knigth(board, Color.BLACK));
-        //bispo
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));       
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
-        //rei
-        placeNewPiece('e', 8, new King(board, Color.BLACK));
-        //peao
+        placeNewPiece('g', 8, new Knigth(board, Color.BLACK));
+        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+       
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
